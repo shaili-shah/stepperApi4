7b2a5016c90f119f20805e7b2ba7c0341b88d525
shaili-shah <90188512+shaili-shah@users.noreply.github.com>
2021-10-04 18:38:57 +0530

    add swagger

--- StepperWebApI/Controllers/UserController.cs
+++ StepperWebApI/Controllers/UserController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 
 namespace StepperWebApI.Controllers
 {
     [Route("User")]
-    public class UserController : Controller
+    public class UserController : ApiController
     {
         // GET: User
         [Route("GetDetails")]
--- StepperWebApI/Global.asax.cs
+++ StepperWebApI/Global.asax.cs
@@ -18,6 +18,8 @@ namespace StepperWebApI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+
         }
     }
 }
